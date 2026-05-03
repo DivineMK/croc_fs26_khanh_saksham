a011b95d94d815994caa39382e8e0384490ad455
Lo Khanh <khanlo@ethz.ch>
2026-05-03 10:47:35 +0200

    feat: add testbench for cordic

---
+++ verilator/cordic.f
@@ -0,0 +1,14 @@
++incdir+../rtl/common_cells/include
++incdir+../rtl/obi/include
+../rtl/common_verification/clk_rst_gen.sv
+../rtl/common_cells/cf_math_pkg.sv
+../rtl/obi/obi_pkg.sv
+../rtl/croc_pkg.sv
+../rtl/user_domain/accumulator.sv
+../rtl/user_domain/TANtable.sv
+../rtl/user_domain/config_sfr.sv
+../rtl/user_domain/control_unit.sv
+../rtl/user_domain/cordic_engine.sv
+../rtl/user_domain/cordic.sv
+../rtl/test/tb_cordic.sv
+
