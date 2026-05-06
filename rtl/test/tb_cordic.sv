e70b73a34188e9fc8dc8f234980cf9e3731e497f
Saksham Kamath <kamathsaksham@gmail.com>
2026-05-06 19:07:50 +0200

    TB modified for drcg testing

--- rtl/test/tb_cordic.sv
+++ rtl/test/tb_cordic.sv
@@ -24,7 +24,8 @@ module tb_cordic #();
       .clk_i,
       .rst_ni,
       .obi_req_i(req),
-      .obi_rsp_o(rsp)
+      .obi_rsp_o(rsp),
+      .drcg_en_i(1'b1) // Enable clock gating for testing
   );
 
   initial begin
@@ -39,32 +40,37 @@ module tb_cordic #();
     @(posedge clk_i);
     // 1. Write to Precision SFR (Addr: 0x0)
     // Set to maximum iterations (e.g., 16)
+    #1;
     req.req = 1'b1;
     req.a.we = 1'b1;
-    req.a.addr = 32'h0;
-    req.a.wdata = 32'h0000_0010; // 16 iterations
+    req.a.addr = 32'h2000_1000 + 32'h0;
+    req.a.wdata = 32'h0000_000d; // 16 iterations
     
     // Wait for the grant indicating request accepted
     wait(rsp.gnt);
     @(posedge clk_i);
+    #1;
     req.req = 1'b0;
     // 2. Write to Operation SFR (Addr: 0x4)
     // Starts the computation. Let's say we want OpMode=0 (Rotation), OpType=0 (Sine), Angle = 30 degrees (mapped)
     // Angle sits in the upper bits [31:16]
     @(posedge clk_i);
+    #1;
     req.req = 1'b1;
     req.a.we = 1'b1;
-    req.a.addr = 32'h4;
-    req.a.wdata = {16'h2000, 16'h0000}; // Sample angle and zeroed OpMode/OpType
+    req.a.addr = 32'h2000_1000 + 32'h4;
+    // req.a.wdata = {20'h10C15, 12'h0000}; // pi/3
+    req.a.wdata = {20'h0C910, 12'h0000}; // pi/4
     
     wait(rsp.gnt);
     @(posedge clk_i);
+    #1;
     req.req = 1'b0;
     // 3. Wait for computation to finish (rvalid goes high)
     wait(rsp.rvalid);
     $display("Time: %0t | CORDIC Output Result: %h", $time, rsp.r.rdata);
     // Finish simulation
-    #100ns;
+    #170ns;
     $finish;
   end
 endmodule
