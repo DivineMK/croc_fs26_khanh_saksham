712845462b9ea006820d9717c4123e1231600f05
Lo Khanh <khanlo@ethz.ch>
2026-05-18 22:34:33 +0200

    openroad: add scripts for power estimation

---
+++ openroad/scripts/06_spef.tcl
@@ -0,0 +1,6 @@
+source scripts/init_tech.tcl
+set extRules ../technology/rcx/IHP_rcx_patterns.rules
+read_def ./out/${proj_name}.def
+define_process_corner -ext_model_index 0 tt
+extract_parasitics -ext_model_file $extRules
+write_spef ./out/${proj_name}.spef
---
+++ openroad/scripts/08_power_stat.tcl
@@ -0,0 +1,21 @@
+source scripts/startup.tcl
+
+# Load the gate-level netlist
+read_verilog out/${proj_name}.v
+# Link the design hierarchy using the top module name
+link_design ${top_design}
+# Load timing constraints
+read_sdc out/${proj_name}.sdc
+# Load extracted parasitics
+read_spef out/${proj_name}.spef
+# Set uniform switching activity rate for all input ports, you may also replace -input with -global
+#set_power_activity -input -activity 0.1
+set_power_activity -input -activity 0.1
+# Set known static inputs (e.g., reset) to zero activity
+set_power_activity -input_port rst_ni -activity 0
+# Generate the statistical power report for the typical corner
+set filename "${report_dir}/07_${proj_name}.power_stat.rpt"
+set fileId [open $filename w]
+close $fileId
+report_puts "Statistical power estimation"
+report_power -corner tt >> $filename
